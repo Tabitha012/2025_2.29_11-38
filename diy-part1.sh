3a8a105a9aef05e106b61b7a92ad7f8d5824d4f1
Tabitha012 <jamineseyi@gmail.com>
2025-03-30 01:07:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
@@ -10,9 +11,55 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
+# 添加 helloworld 软件源
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 添加 passwall 软件源（如果需要）
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 修改默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改默认主机名为 OpenWrt
+sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+# 启用 DHCP 服务
+sed -i 's/option ignore 1/option ignore 0/g' package/network/config/netifd/files/etc/config/dhcp
+
+# 设置默认登录用户名和密码为 root
+sed -i 's/option username.*/option username root/g' package/base-files/files/bin/config_generate
+sed -i 's/option password.*/option password root/g' package/base-files/files/bin/config_generate
+
+# 配置 2.4GHz Wi-Fi
+cat >> package/kernel/mac80211/files/lib/wifi/mac80211.sh <<EOF
+wifi_device \${devid} 'radio0' \
+    type 'mac80211' \
+    channel '6' \
+    band '2g' \
+    htmode 'HT20' \
+    country 'CN' \
+    disabled '0'
+EOF
+
+# 配置 5.0GHz Wi-Fi
+cat >> package/kernel/mac80211/files/lib/wifi/mac80211.sh <<EOF
+wifi_device \${devid} 'radio1' \
+    type 'mac80211' \
+    channel '36' \
+    band '5g' \
+    htmode 'VHT80' \
+    country 'CN' \
+    disabled '0'
+EOF
+
+# 设置 2.4GHz Wi-Fi 名称、密码和安全模式
+uci set wireless.@wifi-iface[0].ssid='OpenWrt_2.4G'
+uci set wireless.@wifi-iface[0].key='12345678'
+uci set wireless.@wifi-iface[0].encryption='sae-mixed'
+uci commit wireless
+
+# 设置 5.0GHz Wi-Fi 名称、密码和安全模式
+uci set wireless.@wifi-iface[1].ssid='OpenWrt_5G'
+uci set wireless.@wifi-iface[1].key='12345678'
+uci set wireless.@wifi-iface[1].encryption='sae-mixed'
+uci commit wireless
